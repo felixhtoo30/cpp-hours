520f5cedb4dea25e6ba6cc981196b57b224dcf69
Felix Htoo <felixhtoo@FHs-MacPro-M3.local>
2025-05-14 23:56:03 +0700

    update: Modify lesson 18: pointer memory leak sample with simple betting game

--- pointers/pointer-18-simple-betting-game.cpp
+++ pointers/pointer-18-simple-betting-game.cpp
@@ -3,9 +3,19 @@
 
 #include <math.h>
 
+int cash = 100;
+
 void Play(int bet)
 {
-    char C[] = {'J', 'Q', 'K'};
+    /** Using normal variable (Stack) */
+    // char C[] = {'J', 'Q', 'K'};
+
+    /** Using malloc (Heap) and Test for Memory Leak */
+    char *C = (char *)malloc(10000 * sizeof(char));
+    C[0] = 'J';
+    C[1] = 'Q';
+    C[2] = 'K';
+
     printf("Shuffling ... \n");
     srand(time(NULL)); // seeding random number generator
 
@@ -19,18 +29,30 @@ void Play(int bet)
         C[y] = temp; // swap characters at position x and y
     }
 
-    // int playerGuess
-    // ...Here...
+    int playerGuess;
+    printf("What is the position of queen - 1, 2 or 3? \n");
+    scanf("%d", &playerGuess);
+    if (C[playerGuess - 1] == 'Q')
+    {
+        cash += 3 * bet;
+        printf("You Win! Result = \"%c %c %c\". Cash = %d \n", C[0], C[1], C[2], cash);
+    }
+    else
+    {
+        cash -= bet;
+        printf("You Lose! Result = \"%c %c %c\". Cash = %d \n", C[0], C[1], C[2], cash);
+    }
 
+    /** Freeing memory to avoid memory leak */
+    free(C);
 }
 
-int cash = 100;
 int main()
 {
     int bet;
     while (cash > 0)
     {
-        printf("What is your bet? $ \n");
+        printf("What is your bet? $");
         scanf("%d", &bet);
         if (bet == 0 || bet > cash)
             break;
